d0b153035271eea632285ef02eece1950ec3f50b
btom7447 <tombenjamin7447@gmail.com>
2025-09-18 20:09:46 +0100

    fixes 1.2

--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -47,7 +47,7 @@ export default function ServicesSection() {
 
     return (
         <section id="services" className="py-10 px-5 lg:p-20 bg-gray-100">
-            <h2 className="text-black text-3xl xl:text-6xl font-light text-center mb-5 lg:mb-10"
+            <h2 className="text-black text-3xl xl:text-6xl font-light text-center mb-5 lg:mb-20"
                 data-aos="fade-down"
                 data-aos-delay="100"
             >
